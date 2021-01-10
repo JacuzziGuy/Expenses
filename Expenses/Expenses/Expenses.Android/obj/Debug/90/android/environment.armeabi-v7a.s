7f81590b44a42ac61b5dddab6d7874c8500d79a7
Jacek Garlicki <thepandaamail@gmail.com>
2021-01-10 16:30:22 +0100

    Poprawione wyświetlanie przycisku usuń

--- Expenses/Expenses/Expenses.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ Expenses/Expenses/Expenses.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"367ceedb-cf7f-4057-8c20-7884ead7795f"
+	.asciz	"24c933f8-bc21-4d4c-b8ad-4aac603d0d98"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
--- Expenses/Expenses/Expenses/Models/ItemModel.cs
+++ Expenses/Expenses/Expenses/Models/ItemModel.cs
@@ -10,6 +10,6 @@ namespace Expenses.Models
 		public string Name { get; set; }
 		public string Price { get; set; }
 		public string Date { get; set; }
-		public string Display { get { return $"{Name}, {Price}zł, {Date}"; } }
+		public string Display { get { return $"Na: {Name},\nCena: {Price}zł,\nZ dnia: {Date}"; } }
 	}
 }
